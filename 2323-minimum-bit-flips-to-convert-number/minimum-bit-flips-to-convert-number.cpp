f3a35a46388b6cf939797c14fabbc038fb9096f7
pritam586 <pritamprajapati666@gmail.com>
2026-03-17 11:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (3.37%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
-    string binary(int n){
-        string ans = "";
-        while(n > 0){
-            int m = n % 2;
-            ans.push_back(m + '0'); 
-            n /= 2;                
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
-
     int minBitFlips(int start, int goal) {
-        string st  = binary(start);
-        string ed = binary(goal);
-
+        int ans = start^goal;
         int count = 0;
-
-        int n = max(st.size(), ed.size());
-
-        while(st.size() < n) st = "0" + st;
-        while(ed.size() < n) ed = "0" + ed;
-
-        for(int i = 0; i < n; i++){
-            if(st[i] != ed[i]){
+        while(ans!=0){
+            int m = ans%2;
+            if(m==1){
                 count++;
             }
+            ans/=2;
         }
         return count;
     }
