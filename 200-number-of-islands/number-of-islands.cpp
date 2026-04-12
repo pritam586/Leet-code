cf12ca76d698b3975b65a54d59e6bf4999e7932f
pritam586 <pritamprajapati666@gmail.com>
2026-04-12 07:40:20 +0530

    Time: 29 ms (42.22%) | Memory: 16.1 MB (96.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -4,14 +4,14 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        // boundary check + water check
+      
         if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0')
             return;
 
-        // mark visited
+       
         grid[i][j] = '0';
 
-        // explore 4 directions
+      
         dfs(grid, i + 1, j);
         dfs(grid, i - 1, j);
         dfs(grid, i, j + 1);
@@ -26,8 +26,8 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    count++;        // found new island
-                    dfs(grid, i, j); // mark whole island
+                    count++;        
+                    dfs(grid, i, j); 
                 }
             }
         }
