dbd94bb5b67cdab43c4f0cb6e0a3a51c6715827f
pritam586 <pritamprajapati666@gmail.com>
2026-04-19 15:30:20 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (14.97%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+     int solve(int i , int j , int m , int n ,vector<vector<int>>& grid , vector<vector<int>> &dp ){
+        if(i >= m || j >= n) return 1e9;
+
+        // destination
+        if(i == m-1 && j == n-1) return grid[i][j];
+
+        // memo
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int down  = solve(i+1, j, m, n, grid, dp);
+        int right = solve(i, j+1, m, n, grid, dp);
+
+        return dp[i][j] = grid[i][j] + min(down, right);
+     }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(0 , 0 , m , n , grid , dp);
+    }
+};
