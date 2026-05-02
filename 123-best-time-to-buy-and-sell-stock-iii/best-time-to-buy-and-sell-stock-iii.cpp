276328eb3991c7fe0192c2594e10dd48b4b9e6d1
pritam586 <pritamprajapati666@gmail.com>
2026-05-02 13:33:55 +0530

    Time: 391 ms (44.61%) | Memory: 211.6 MB (34.27%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int cal(vector<int>& prices, int idx, int buy, int n,
-            vector<vector<vector<int>>>& dp, int cap) {
-        
-        if (idx == n || cap == 0) return 0;
-
-        if (dp[idx][buy][cap] != -1) return dp[idx][buy][cap];
-
-        int profit = 0;
-
-        if (buy) {
-            profit = max(
-                -prices[idx] + cal(prices, idx + 1, 0, n, dp, cap),
-                cal(prices, idx + 1, 1, n, dp, cap)
-            );
-        } else {
-            profit = max(
-                prices[idx] + cal(prices, idx + 1, 1, n, dp, cap - 1),
-                cal(prices, idx + 1, 0, n, dp, cap)
-            );
-        }
-
-        return dp[idx][buy][cap] = profit;
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return cal(prices, 0, 1, n, dp, 2);
+
+        
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+
+                    if (buy) {
+                        dp[idx][buy][cap] = max(
+                            -prices[idx] + dp[idx+1][0][cap],   
+                            dp[idx+1][1][cap]                   
+                        );
+                    } else {
+                        dp[idx][buy][cap] = max(
+                            prices[idx] + dp[idx+1][1][cap-1],  
+                            dp[idx+1][0][cap]                   
+                        );
+                    }
+
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 };
