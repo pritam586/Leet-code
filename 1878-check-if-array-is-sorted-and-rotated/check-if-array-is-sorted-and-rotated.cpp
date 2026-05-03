388f4ac54653c8123f2762bc17e6072bf9a30668
pritam586 <pritamprajapati666@gmail.com>
2026-05-03 12:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (98.49%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -8,21 +8,22 @@ public:
             if(nums[i] > nums[(i+1) % n]){
                 count++;  
 
-                int curr = nums[i];
-                int j = nums[(i+1) % n];
-                int idx = i;
+            //     int curr = nums[i];
+            //     int j = nums[(i+1) % n];
+            //     int idx = i;
 
-                for(int k = idx ; k >= 0 ; k--){
-                    if(nums[k] < j){
-                        return false;
-                    }
-                }
+            //     for(int k = idx ; k >= 0 ; k--){
+            //         if(nums[k] < j){
+            //             return false;
+            //         }
+            //     }
 
-                for(int k = idx ; k < n ; k++){
-                    if(nums[k] > curr){
-                        return false;
-                    }
-                }
+            //     for(int k = idx ; k < n ; k++){
+            //         if(nums[k] > curr){
+            //             return false;
+            //         }
+            //     }
+            // }
             }
         }
         if(count > 1) return false;
