f8691766dcf098cf29c33ca30220690cd9674bfa
pritam586 <pritamprajapati666@gmail.com>
2026-05-21 16:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (6.97%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+
+    void traverse(int st,vector<vector<int>>& adj,vector<bool>& isVisited) {
+
+        queue<int> q;
+
+        q.push(st);
+        isVisited[st] = true;
+
+        while(!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            for(auto it : adj[node]) {
+
+                if(!isVisited[it]) {
+
+                    isVisited[it] = true;
+                    q.push(it);
+                }
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+
+        int n = isConnected.size();
+
+        vector<vector<int>> adj(n);
+
+        // Create adjacency list
+        for(int i = 0; i < n; i++) {
+
+            for(int j = 0; j < n; j++) {
+
+                if(isConnected[i][j] == 1 && i != j) {
+
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+
+        vector<bool> isVisited(n, false);
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(!isVisited[i]) {
+
+                count++;
+
+                traverse(i, adj, isVisited);
+            }
+        }
+
+        return count;
+    }
+};
