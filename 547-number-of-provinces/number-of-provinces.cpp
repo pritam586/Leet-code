f911d372d6627af04ad92340feb34a4bca4a540d
pritam586 <pritamprajapati666@gmail.com>
2026-07-22 15:59:48 +0545

    Time: 4 ms (24.68%) | Memory: 20.8 MB (21.91%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-
-    void traverse(int st,vector<vector<int>>& adj,vector<bool>& isVisited) {
-
+    void traverse(int st, vector<vector<int>>& adj, vector<bool>& isVisited) {
         queue<int> q;
-
         q.push(st);
         isVisited[st] = true;
 
-        while(!q.empty()) {
-
+        while (!q.empty()) {
             int node = q.front();
             q.pop();
 
-            for(auto it : adj[node]) {
-
-                if(!isVisited[it]) {
-
+            for (auto it : adj[node]) {
+                if (!isVisited[it]) {
                     isVisited[it] = true;
                     q.push(it);
                 }
@@ -25,18 +19,13 @@ public:
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-
         int n = isConnected.size();
 
         vector<vector<int>> adj(n);
 
-        // Create adjacency list
-        for(int i = 0; i < n; i++) {
-
-            for(int j = 0; j < n; j++) {
-
-                if(isConnected[i][j] == 1 && i != j) {
-
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
@@ -44,16 +33,12 @@ public:
         }
 
         vector<bool> isVisited(n, false);
-
         int count = 0;
 
-        for(int i = 0; i < n; i++) {
-
-            if(!isVisited[i]) {
-
-                count++;
-
+        for (int i = 0; i < n; i++) {
+            if (!isVisited[i]) {
                 traverse(i, adj, isVisited);
+                count++;
             }
         }
 
