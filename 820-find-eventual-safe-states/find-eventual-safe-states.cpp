3e2720150db96b1db751767f37010c2e888e9153
pritam586 <pritamprajapati666@gmail.com>
2026-07-30 21:10:08 +0545

    Time: 57 ms (19.77%) | Memory: 66.4 MB (20.11%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+
+        int n = graph.size();
+
+        vector<vector<int>> revGraph(n);
+        vector<int> indegree(n, 0);
+
+        // Reverse the graph
+        for (int i = 0; i < n; i++) {
+            for (auto it : graph[i]) {
+                revGraph[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        vector<int> ans;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for (auto it : revGraph[node]) {
+                indegree[it]--;
+
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
