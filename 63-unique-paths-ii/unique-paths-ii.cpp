f3f62864e7e964fbf8e9306e41f8c249beaae1d5
pritam586 <pritamprajapati666@gmail.com>
2026-08-24 22:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (55.45%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
+    int cal(vector<vector<int>>& obstacleGrid , int m, int n, vector<vector<int>>& dp, int row, int col) {
+        // Out of boundary
+        if (row >= m || col >= n || obstacleGrid[row][col] ==1)
+            return 0;
 
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-
-       
-        if(obstacleGrid[0][0] == 1) return 0;
-        dp[0][0] = 1;
-
-       
-        for(int i = 1; i < n; i++){
-            if(obstacleGrid[i][0] == 0)
-                dp[i][0] = dp[i-1][0];
-        }
-
+        // Reached destination
+        if (row == m - 1 && col == n - 1)
+            return 1;
      
-        for(int j = 1; j < m; j++){
-            if(obstacleGrid[0][j] == 0)
-                dp[0][j] = dp[0][j-1];
-        }
 
-        
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j < m; j++){
-                if(obstacleGrid[i][j] == 0){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            
-            }
-        }
+        if (dp[row][col] != -1)
+            return dp[row][col];
 
-        return dp[n-1][m-1];
+        int down = cal(obstacleGrid,m, n, dp, row + 1, col);
+        int right = cal(obstacleGrid,m, n, dp, row, col + 1);
+
+        return dp[row][col] = down + right;
+    }
+
+   
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        return cal(obstacleGrid , m , n , dp , 0 , 0 );
     }
 };
