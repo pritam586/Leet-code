3642e0817424f879db929009caa150fcfb7ab7e4
pritam586 <pritamprajapati666@gmail.com>
2026-08-25 20:04:14 +0530

    Time: 3 ms (26.64%) | Memory: 52.3 MB (57.75%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIndex = 0;
+        for(int i  = 0 ; i < nums.size() ; i++){
+            if (i > maxIndex) return false;
+            maxIndex = max(maxIndex ,i+ nums[i]);
+            if(maxIndex>=nums.size()-1) return true;
+        }
+        return false;
+    }
+};
